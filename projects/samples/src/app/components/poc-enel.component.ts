83815d89e2508befc4dfaec0ef79c57fb2c2a557
daniele-rabdo <daniele.casali@spindox.it>
2024-09-17 14:31:35 +0200

    added legend

--- projects/samples/src/app/components/poc-enel.component.ts
+++ projects/samples/src/app/components/poc-enel.component.ts
@@ -33,7 +33,96 @@ export class PocEnelComponent {
   items: FamItemConfig[] = [];
   annotations: (LabelAnnotationConfig | HighlightPathAnnotationConfig)[] = [];
   showLegend: boolean = false;
-  legendItems: { name: string; color: string }[] = [];
+  legendItems: { name: string; color: string }[] = [
+    {
+      name: 'ARS',
+      color: '#42fe00',
+    },
+    {
+      name: 'AUD',
+      color: '#90fe00',
+    },
+    {
+      name: 'BRL',
+      color: '#fc0101',
+    },
+    {
+      name: 'CAD',
+      color: '#a60301',
+    },
+    {
+      name: 'CLP',
+      color: '#070501',
+    },
+    {
+      name: 'COP',
+      color: '#640501',
+    },
+    {
+      name: 'CRC',
+      color: '#b40501',
+    },
+    {
+      name: 'EUR',
+      color: '#a20d01',
+    },
+    {
+      name: 'GTQ',
+      color: '#041501',
+    },
+    {
+      name: 'HNL',
+      color: '#061801',
+    },
+    {
+      name: 'INR',
+      color: '#cd1b01',
+    },
+    {
+      name: 'KRW',
+      color: '#d02301',
+    },
+    {
+      name: 'MXN',
+      color: '#032c01',
+    },
+    {
+      name: 'PEN',
+      color: '#f93401',
+    },
+    {
+      name: 'RON',
+      color: '#b13d01',
+    },
+    {
+      name: 'RUB',
+      color: '#5f3e01',
+    },
+    {
+      name: 'SAR',
+      color: '#c43f01',
+    },
+    {
+      name: 'TRY',
+      color: '#9b4501',
+    },
+    {
+      name: 'USD',
+      color: '#664901',
+    },
+    {
+      name: 'UYU',
+      color: '#314a01',
+    },
+    {
+      name: 'ZAR',
+      color: '#0b5a01',
+    },
+    {
+      name: 'ZMW',
+      color: '#845b01',
+    },
+  ];
   jsonTextArea = '';
   errorMsg = '';
 
